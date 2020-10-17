7af1f6f9c1e05bd1f41274df646c1fdd7e563e06
Yan Lin Aung <yanlinaung30@gmail.com>
2020-10-17 14:26:04 +0630

    Add projects menu and pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -69,6 +69,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
+              <Link className="navbar-item" to="/projects">
+                Projects
+              </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
@@ -76,7 +79,7 @@ const Navbar = class extends React.Component {
                 Form Examples
               </Link>
             </div>
-            <div className="navbar-end has-text-centered">
+            {/* <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
                 href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
@@ -87,7 +90,7 @@ const Navbar = class extends React.Component {
                   <img src={github} alt="Github" />
                 </span>
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </nav>
