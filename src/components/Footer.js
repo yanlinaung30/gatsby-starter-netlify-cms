2a26e47941ea92f12dcd1ff4b1b61defd42d4277
Yan Lin Aung <yanlinaung30@gmail.com>
2020-10-17 16:35:08 +0630

    Change footer logo, change links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+// import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -13,7 +13,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
+            src="img/logo.jpg"
             alt="Moon City"
             style={{ width: '14em', height: '10em' }}
           />
@@ -35,16 +35,21 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
+                      <Link className="navbar-item" to="/services">
+                        Services
                       </Link>
                     </li>
+                    {/* <li>
+                      <Link className="navbar-item" to="/project">
+                        Projects
+                      </Link>
+                    </li> */}
                     <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
+                      <Link className="navbar-item" to="/careers">
+                        Careers
                       </Link>
                     </li>
-                    <li>
+                    {/* <li>
                       <a
                         className="navbar-item"
                         href="/admin/"
@@ -53,7 +58,7 @@ const Footer = class extends React.Component {
                       >
                         Admin
                       </a>
-                    </li>
+                    </li> */}
                   </ul>
                 </section>
               </div>
@@ -61,8 +66,8 @@ const Footer = class extends React.Component {
                 <section>
                   <ul className="menu-list">
                     <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                      <Link className="navbar-item" to="/project">
+                        Projects
                       </Link>
                     </li>
                     <li>
