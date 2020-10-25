7cff4819342da23a55d5de21bdb014b544564568
Yan Lin Aung <yanlinaung30@gmail.com>
2020-10-25 14:53:15 +0630

    Add Service Page Preview, Hide Blog and Product Page Previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,11 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from './preview-templates/AboutPagePreview'
+// import BlogPostPreview from './preview-templates/BlogPostPreview'
+// import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ServicePagePreview from './preview-templates/ServicePagePreview'
 import ProjectPostPreview from './preview-templates/ProjectPostPreview'
 
 CMS.registerMediaLibrary(uploadcare)
@@ -13,6 +14,7 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+// CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('services', ServicePagePreview)
 CMS.registerPreviewTemplate('project', ProjectPostPreview)
